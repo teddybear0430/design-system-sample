4f272709021d005402a514ba61632629f332ea37
teddybear0430 <teddybear0430@hotmail.com>
2022-07-10 22:20:31 +0900

    refactor: generateSvgIconPath

--- packages/react/components/Icon/generateSvgIconPath.ts
+++ packages/react/components/Icon/generateSvgIconPath.ts
@@ -1,7 +1,17 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-// iconsディレクトリにある全てのSVGの名前を取得
+/**
+ * svgディレクトリ内のSVGから以下の処理を生成する処理
+ *
+ * 1, Iconコンポーネントのmask-imageに渡すパス
+ * 2, Iconコンポーネントのnameの型
+ *
+ */
+
+/**
+ * iconsディレクトリにある全てのSVGファイルの名前を配列で取得
+ */
 const getAllIconSvgName = () => {
   const dir = path.join(__dirname, '/svg');
   const fileNameList = fs.readdirSync(dir);
@@ -11,50 +21,65 @@ const getAllIconSvgName = () => {
   return fileNameList;
 };
 
-// 取得した全てのSVGファイルの拡張子を除去する
-const AllIconSvgremoveExtension = () => {
+/**
+ * 取得した全てのSVGファイルの拡張子(.svg)を除去する
+ */
+const allIconSvgRemoveExtension = () => {
   return getAllIconSvgName().map((icon) => icon.replace(/\.svg$/, ''));
 };
 
-// SVGをimportする記述をSVGディレクトリに存在するSVGから生成
-const svgImport = (iconName: string) => {
-  console.log(`import ${iconName} from './svg/${iconName}'`);
+/**
+ * SVGをimportする記述をSVGディレクトリに存在するSVGから生成
+ *
+ * @param {string} iconName - アイコンの名前 (例: Add, Arrow)
+ */
+const generateSvgImportStatement = (iconName: string) => {
+  console.info(`import ${iconName} from './svg/${iconName}'`);
+
   return `import ${iconName} from './svg/${iconName}.svg';`;
 };
 
-// 取得した全てのSVGからアイコンコンポーネントの型を生成
-const svgType = () => {
-  const icons = AllIconSvgremoveExtension();
+/**
+ * 取得した全てのSVGからIconのnameに渡すことができる型を生成
+ */
+const generateSvgNameType = () => {
+  const icons = allIconSvgRemoveExtension();
   return `export const svgs = ${JSON.stringify(icons)} as const
 export type SvgType = typeof svgs[number];
 `;
 };
 
-// svgの名前とSVGのkey, valueの組み合わせ
+/**
+ * SVGの名前からSVGのkey, valueの組み合わせを生成
+ *
+ * @param {string} iconName - アイコンの名前 (例: Add, Arrow)
+ */
 const svgKeyValue = (iconName: string) => {
   return `${iconName}: ${iconName}, `;
 };
 
-// SVGアイコンのパスとアイコンの型を生成
+/**
+ * svgIconPath.tsに書き出す内容を生成
+ */
 const generateSwitchIconPathLogic = () => {
-  const importStatement = AllIconSvgremoveExtension()
+  const svgImportStatement = allIconSvgRemoveExtension()
     .map((icon) => {
-      return svgImport(icon);
+      return generateSvgImportStatement(icon);
     })
     .join('');
 
-  const iconType = svgType();
+  const svgNameType = generateSvgNameType();
 
-  const svgObj = AllIconSvgremoveExtension()
+  const svgObj = allIconSvgRemoveExtension()
     .map((icon) => {
       return svgKeyValue(icon);
     })
     .join('');
 
   // `や$はバックスラッシュを使って文字列として扱えるようにする
-  return `${importStatement}
+  return `${svgImportStatement}
 
-${iconType}
+${svgNameType}
 
 const svgObj = {${svgObj}};
 
@@ -72,8 +97,9 @@ export const getSvgIconPath = (iconName: SvgType) => {
 };
 
 // ファイルの書き込み処理
-const writeFile = (path: string, data: string) => {
-  fs.writeFile(path, data, (er) => {
+const writeFile = () => {
+  const filePath = path.join(__dirname, '/svgIconPath.ts');
+  fs.writeFile(filePath, generateSwitchIconPathLogic(), (er) => {
     if (er) {
       console.error('予期しないエラーが発生しました', er);
       throw er;
@@ -83,10 +109,4 @@ const writeFile = (path: string, data: string) => {
   });
 };
 
-// メインの処理
-const mainFunc = () => {
-  const filePath = path.join(__dirname, '/svgIconPath.ts');
-  writeFile(filePath, generateSwitchIconPathLogic());
-};
-
-mainFunc();
+writeFile();
